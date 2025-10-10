3b2879c6ff5b6a5c1d5b7d62b384dd02c16be094
Faizulmehram <faiz.ul.mehram111@gmail.com>
2025-10-11 00:16:05 +0500

    all errors fixed last page remaining

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,5 +1,3 @@
-
-
 export default function SearchBar() {
   return (
   <div className="flex flex-col md:flex-row items-center gap-3 text-[15px]">
@@ -8,7 +6,7 @@ export default function SearchBar() {
           {/* Removed search image from search bar */}
             <span className="absolute left-3 top-1/2 transform -translate-y-1/2 pointer-events-none">
               <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#94a3b8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#94a3b8" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
               </svg>
             </span>
             <input
